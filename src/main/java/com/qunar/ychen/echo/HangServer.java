7acddf031d9ac7ddfd8fcdd6cd5f0864a26970e8
yuxiang.chen <yuxiang.chen@jiulvxing.com>
2016-11-07 15:26:14 +0800

    Fix

--- src/main/java/com/qunar/ychen/echo/HangServer.java
+++ src/main/java/com/qunar/ychen/echo/HangServer.java
@@ -1,5 +1,7 @@
 package com.qunar.ychen.echo;
 
+import com.qunar.ychen.utils.SleepUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,18 +19,12 @@ public class HangServer extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         String sleep = req.getParameter("sleep");
-        Thread.sleep(Long.parseLong(sleep));
+        SleepUtil.sleep(sleep);
 
-        String echo = req.getParameter("name");
-        String response;
 
-        if (echo == null || echo.length() == 0) {
-            response = "{\"status\":1,\"message\":\"error\",\"data\":\"name is null\"}";
-            log("echo is blank, default response is:" + response);
-        } else {
-            response = "{\"status\":0,\"message\":\"success\",\"data\":\"hello " + echo + "\"}";
-            log("echo is " + echo + ", response is :" + response);
-        }
+        String response = "{\"status\":0,\"message\":\"success\",\"data\":\"sleep" + sleep + "millis" + "\"}";
+        log("response is :" + response);
+
         response = response.concat(System.lineSeparator());
         resp.setContentType("application/json; charset=UTF-8");
         PrintWriter out = resp.getWriter();
